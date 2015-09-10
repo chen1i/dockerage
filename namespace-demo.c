ba567399230f13583f9b8794636c6326f65dc349
Li Chen <kyrrechen@gmail.com>
2015-09-10 14:43:16 +0800

    now support fs namespace. ps in container is ioslated as well

--- namespace-demo.c
+++ namespace-demo.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
 #include <signal.h>
@@ -19,6 +20,9 @@ int container_main(void* arg)
 {
   printf("Container [%5d] - inside the container!\n", getpid());
   sethostname("container", 10);
+
+  // remount proc filesystem
+  system("mount -t proc proc /proc");
   /* 直接执行一个shell，以便我们观察这个进程空间里的资源是否被隔离了 */
   execv(container_args[0], container_args);
   printf("Something's wrong!\n");
@@ -31,10 +35,13 @@ int main()
   /* 调用clone函数，其中传出一个函数，还有一个栈空间的（为什么传尾指针，因为栈是反着的） */
   int container_pid = clone(container_main,
                             container_stack+STACK_SIZE,
-                            CLONE_NEWUTS | CLONE_NEWIPC |CLONE_NEWPID | SIGCHLD,
+                            CLONE_NEWUTS | CLONE_NEWIPC |CLONE_NEWPID |CLONE_NEWNS | SIGCHLD,
                             NULL);
   /* 等待子进程结束 */
   waitpid(container_pid, NULL, 0);
   printf("Parent - container stopped!\n");
+
+  //remount back to old /proc filesystem
+  system("mount -t proc proc /proc");
   return 0;
 }
