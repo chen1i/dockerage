84d5e088c9fe805d2e3694ea8e71649bd80a51c2
Li Chen <kyrrechen@gmail.com>
2015-09-10 14:23:06 +0800

    PID namespace, you can see pid after clone changed to 1, but ps command still show the real PID, to correct this, we need namespace fs

--- namespace-demo.c
+++ namespace-demo.c
@@ -17,7 +17,7 @@ char* const container_args[] = {
 
 int container_main(void* arg)
 {
-  printf("Container - inside the container!\n");
+  printf("Container [%5d] - inside the container!\n", getpid());
   sethostname("container", 10);
   /* 直接执行一个shell，以便我们观察这个进程空间里的资源是否被隔离了 */
   execv(container_args[0], container_args);
@@ -27,11 +27,11 @@ int container_main(void* arg)
 
 int main()
 {
-  printf("Parent - start a container!\n");
+  printf("Parent [%5d] - start a container!\n", getpid());
   /* 调用clone函数，其中传出一个函数，还有一个栈空间的（为什么传尾指针，因为栈是反着的） */
   int container_pid = clone(container_main,
                             container_stack+STACK_SIZE,
-                            CLONE_NEWUTS | CLONE_NEWIPC | SIGCHLD,
+                            CLONE_NEWUTS | CLONE_NEWIPC |CLONE_NEWPID | SIGCHLD,
                             NULL);
   /* 等待子进程结束 */
   waitpid(container_pid, NULL, 0);
